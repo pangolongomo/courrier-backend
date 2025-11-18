1565a99d37c7c850246a4bce336610d2e43c5160
Matthieu Nzinga <matthieunzinga@gmail.com>
2025-11-18 12:40:11 +0100

    feat(auth): add auth controller (register/login)

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,21 +1,19 @@
-const AuthService = require('../services/auth.service');
-
+const AuthService = require("../services/auth.service");
 
 module.exports.register = async (req, res) => {
-try {
-const user = await AuthService.register(req.body);
-res.status(201).json({ user });
-} catch (err) {
-res.status(400).json({ error: err.message });
-}
+  try {
+    const user = await AuthService.register(req.body);
+    res.status(201).json({ user });
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
 };
 
-
 module.exports.login = async (req, res) => {
-try {
-const result = await AuthService.login(req.body);
-res.json(result);
-} catch (err) {
-res.status(401).json({ error: err.message });
-}
+  try {
+    const result = await AuthService.login(req.body);
+    res.json(result);
+  } catch (err) {
+    res.status(401).json({ error: err.message });
+  }
 };
